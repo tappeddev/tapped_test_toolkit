d34fb6bd8c011b9f621e96b3a9be6c07933e4be8
Stefan Schaller <stefan.schaller@tapped.dev>
2024-01-03 06:46:24 +0100

    upgrade packages

--- lib/src/integration_test/option/integration_test_android_option.freezed.dart
+++ lib/src/integration_test/option/integration_test_android_option.freezed.dart
@@ -219,7 +219,7 @@ class _$IntegrationTestAndroidOptionImpl extends _IntegrationTestAndroidOption {
   }
 
   @override
-  bool operator ==(dynamic other) {
+  bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$IntegrationTestAndroidOptionImpl &&
